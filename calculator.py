85bc13335e1da47a1c2abaa40d01a8d4216c0224
Heather1337 <heatherlynn1337@gmail.com>
2020-09-30 16:12:52 -0700

    created while loop and tokenized string, and began conditionals

--- calculator.py
+++ calculator.py
@@ -4,4 +4,26 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+# Create a function that takes in a users input and returns an float 
+# Create a loop 
+while True:
+# Tokenize a string
+    input_string = input("Enter your equation")
+    tokens = input_string.split(' ')
+
+    operator = tokens[0]
+    num1 = tokens[1]
+
+    if len(tokens) > 2:
+        num2 = tokens[2]
+    
+# Create conditional statements to compare users input to our tokens
+    if operator == "q":
+        break
+    
+
+# Based on the users input we will call the necessary functions
+# Print the end result 
+
+
+
