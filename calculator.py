bc76fc6d28923159eecc9c9ba778ba82cb6b0cc5
vagrant <vagrant@vagrant.vm>
2020-10-01 00:17:06 +0000

    Adding error message for case when 2nd input is not integer

--- calculator.py
+++ calculator.py
@@ -8,22 +8,31 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 # Create a loop 
 while True:
 # Tokenize a string
-    input_string = input("Enter your equation")
+    input_string = input("Enter your equation: ")
     tokens = input_string.split(' ')
     result = None
 
+
+
     operator = tokens[0]
     if operator == "q":
-    break
+        break
 
+    #Checking for input of 1 and returning error 
     if len(tokens) < 2:
         print('Invalid entry, please try again.')
-    num1 = float(tokens[1])
 
+    #Assigning first input digit to var num1
+    num1 = tokens[1]
+    if not num1.isdigit():
+        print("Please provide a number")
+
+    #Checking if input is 3 or more 
     elif len(tokens) > 2:
-        num2 = float(tokens[2])
-    
+        num2 = tokens[2]
 
+
+    
     
 # Create conditional statements to compare users input to our tokens
 
@@ -41,7 +50,7 @@ while True:
         result = divide(num1, num2)
 
     elif operator == "square":
-        result = square(num1)
+        result = square(float(num1))
 
     elif operator == "cube":
         result = cube(num1)
@@ -53,6 +62,7 @@ while True:
         result = mod(num1, num2)
     
     print(result)
+   
 # Based on the users input we will call the necessary functions
 # Print the end result 
 
